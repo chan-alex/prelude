d768c35c535129bdaad3d2d1c26471dd0ea04354
Alex Chan <achan@cloudflare.com>
2019-01-28 08:56:21 +0800

    checkpoint

--- personal/personal.el
+++ personal/personal.el
@@ -9,3 +9,12 @@
 
 (setq python-shell-interpreter "python3"
       python-shell-interpreter-args "-i")
+
+
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+(setq org-agenda-files (list "~/org/todo/"
+                             "~/org/cf_notes/"))      
